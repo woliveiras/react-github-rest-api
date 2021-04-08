61b3e5b48af572e8bde4770589dec8f7dbaad97e
William Oliveira <w.oliveira542@gmail.com>
2021-04-08 11:44:22 -0300

    feat(cards): add BaseCard

---
+++ src/components/BaseCard/BaseCard.jsx
@@ -0,0 +1,11 @@
+import './BaseCard.sass'
+
+const BaseCard = ({children}) => {
+    return (
+        <div className='baseCard'>
+            {children}
+        </div>
+    )
+}
+
+export default BaseCard
