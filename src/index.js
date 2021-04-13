6caa8d4101d5d06804ed00975fc6c9fd2bab0571
William Oliveira <w.oliveira542@gmail.com>
2021-04-13 08:22:09 -0300

    refac(sass): add mixins

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 
-import './reset.sass'
-import './base.sass'
+import './sass/main.sass'
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
 
 import App from './App'
